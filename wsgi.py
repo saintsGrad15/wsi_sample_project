2a5c909b0e08b4ef07274817107da6962985fc4d
John Carrell <john.carrell@us.ibm.com>
2018-11-03 15:36:22 -0500

    Add db and API invocation to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app as application
+from app import db_connection
+
+from api import register_apis
+
+register_apis(application, db_connection)
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
