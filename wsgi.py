4581f492467ef0861b2df19c839de78da50fb4c4
John Carrell <john.carrell@us.ibm.com>
2018-11-07 07:51:02 -0600

    Pass the db not the class instance

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from app import db_connection_manager
 
 from api import register_apis
 
-register_apis(application, db_connection_manager)
+register_apis(application, db_connection_manager.db)
 
 if __name__ == "__main__":
     application.run()
